c8f0fdb9604c13a1b9d52c141ab6f27a9059a7be
Jaewook Jung <jw0213.jung@samsung.com>
2018-06-19 14:11:50 +0900

    Updated unittest script for code coverage
    Signed-off-by: Jaewook Jung <jw0213.jung@samsung.com>

--- unittest.sh
+++ unittest.sh
@@ -18,10 +18,10 @@
 #!/bin/bash
 
 PROJECT_ROOT=$(pwd)
-CAML_TARGET_ARCH=x86_64
 
 function build(){
-    scons TARGET_OS=linux TARGET_ARCH=${CAML_TARGET_ARCH}
+    cd $PROJECT_ROOT
+    ./build.sh --build_mode=debug
     if [ $? -ne 0 ]; then 
         echo -e "\033[31m"Build failed"\033[0m" 
         exit 1 
@@ -29,13 +29,33 @@ function build(){
 }
 
 function run_test(){
-    cd out/linux/${CAML_TARGET_ARCH}/release/unittests
-    export LD_LIBRARY_PATH=${PROJECT_ROOT}/dependencies/datamodel-aml-cpp/out/linux/${CAML_TARGET_ARCH}/release/
+    cd $PROJECT_ROOT/out/linux/x86_64/debug/unittests
+    export LD_LIBRARY_PATH=${PROJECT_ROOT}/dependencies/datamodel-aml-cpp/out/linux/x86_64/debug
     ./caml_rep_test
     if [ $? -ne 0 ]; then 
         echo -e "\033[31m"Unittests failed"\033[0m" 
         exit 1 
     fi
+}
+
+function coverage() {
+    cd $PROJECT_ROOT
+    if [ ! -d "./coverage" ]; then
+        mkdir coverage
+    fi
+
+    gcovr -r . \
+            -e "build_common.*" \
+            -e "coverage.*" \
+            -e "dependencies.*" \
+            -e "docs.*" \
+            -e "extlibs.*" \
+            -e "out.*" \
+            -e "protobuf.*" \
+            -e "samples.*" \
+            -e "unittests.*" \
+            -e "src/logger.*" \
+            --html --html-details -o ./coverage/report.html
 }
 
 echo -e "Building CAML DataModel library("${CAML_TARGET_ARCH}").."
@@ -45,5 +65,9 @@ echo -e "Done building CAML DataModel library("${CAML_TARGET_ARCH}")"
 echo -e "Running CAML DataModel Unittests"
 run_test
 echo -e "Done Unittests"
+
+if [ "$1" == --coverage ]; then
+    coverage
+fi
 
 exit 0
